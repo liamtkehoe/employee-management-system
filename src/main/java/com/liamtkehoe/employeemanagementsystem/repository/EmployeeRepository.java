ae2bf92700b542741745c92536253d42998951a6
liamtkehoe <liamtkehoe@gmail.com>
2022-06-03 15:54:09 -0400

    created employee table and entity

---
+++ src/main/java/com/liamtkehoe/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.liamtkehoe.employeemanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.liamtkehoe.employeemanagementsystem.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>
+{
+
+}
