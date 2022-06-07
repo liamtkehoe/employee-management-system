357cdf4853cca85a91dcc7c0e3700aaed74348ac
liamtkehoe <liamtkehoe@gmail.com>
2022-06-07 18:00:16 -0400

    fixed CORS issue

--- src/main/java/com/liamtkehoe/employeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/com/liamtkehoe/employeemanagementsystem/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.liamtkehoe.employeemanagementsystem.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.liamtkehoe.employeemanagementsystem.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200") //fixes blocked by CROS policy issue when attempting to access database from angular
 public class EmployeeController 
 {
 	@Autowired
